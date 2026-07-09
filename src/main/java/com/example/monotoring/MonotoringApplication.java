aa6410dac95cb7d3bf2c8eed7d667aa74f7152cf
nourhabibi <nour.habibi@etudiant-isi.utm.tn>
2026-07-09 15:17:12 +0100

    Backend Commit

---
+++ src/main/java/com/example/monotoring/MonotoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.monotoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonotoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonotoringApplication.class, args);
+    }
+
+}
